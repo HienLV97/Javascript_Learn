bfef0a177dd2afbdbf9d88e5654c0dd45459b1bd
Le Vinh Hien <levinhhien@Les-MacBook-Air.local>
2022-12-11 19:10:07 +0700

    test

---
+++ main.js
@@ -0,0 +1,75 @@
+//khai bao bien
+
+var age = 26;
+
+// Call ham thong bao
+// alert(fullName)
+// alert(age)
+
+//console.log(fullName)
+
+//confirm('Hãy xác nhận rằng Hiển đẹp trai')
+
+//prompt('Xac nhan bạn đủ tuổi:')
+
+// setTimeout(function(){
+//     alert('Hien dep trai')
+// }, 2000)
+
+//console.log(age)
+
+//setInterval như settimeout nhưng lập lại liên tục theo time ấn định 
+// setInterval(function(){
+//     console.log('Hien dep trai')
+// }, 2000)
+
+// var a = 'abc';
+// var b = 'def';
+// var space
+
+// var c = a + ' ' + b;
+// console.log(c)
+
+// var sochan = 5
+// var so2 = 2;
+// var c = sochan % so2
+// if (c == 0 ){
+//     console.log('so chan')
+// } else{
+//     console.log('so le')
+// }
+
+var number2 = 5
+var number = 5
+var output = number-- + number-- + number-- + number--;
+var output2 = number2 + --number2 + --number2 + --number2
+
+
+// console.log(number)
+// console.log(output) 
+// console.log(output2)
+ 
+var a = 'abc'
+var b = 'cde'
+var c = 'z'
+var result = 'aZ' || 'Zz' || undefined || 'cc' || 'zz'
+//var result ='A' && 'B' && 'C' || 'D'
+//console.log(result)
+
+
+var ten = 'Hien'
+var ho = 'Le'
+var lot = 'Vinh'
+var fullName = '   Hien JS JS abc xasd acsc JS JS J S   ';
+
+// console.log(fullName.search('JS'))
+// console.log(fullName)
+// //console.log(fullName.slice(-3,-1 ))
+// console.log(fullName.replace(/JS/g,'JavaScript'))
+
+// console.log(fullName.toUpperCase())
+// console.log(fullName.toLowerCase())
+// console.log(fullName.trim())
+// var languages = 'Javascript, PHP, Ruby'
+// console.log(languages.split(''))
+console.log(fullName.trim()[3])
